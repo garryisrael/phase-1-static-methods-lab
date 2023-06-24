4bee49ae34dd2da2f541efa704703b7cc3e4d705
Garry <garryisrael02@gmail.com>
2023-06-24 06:13:55 -0400

    creates static methods

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
 class Formatter {
-  //add static methods here
+  static capitalize(word){
+    return word.charAt(0).toUpperCase()+word.slice(1)
+  }
+
+  static sanitize(sentence){
+    return sentence.replace(/[^\w\-' ]/g,'')
+  }
+
+  static titleize(sentence){
+    const exception = {
+      the: "the",
+      a: "a",
+      an: "an",
+      but: "but",
+      of: "of",
+      and: "and",
+      for: "for",
+      at: "at",
+      by: "by", 
+      and: "and", 
+      from: "from"
+    } 
+    sentence = sentence[0].toUpperCase()+sentence.slice(1)
+     return sentence.split(" ").map(word =>{
+      return exception[word]  ? word :  word.charAt(0).toUpperCase()+word.slice(1)
+    }).join(" ")
+  }
+    
+    
+    
+   //another way of doing the same thing     
+    /*let finalArray = []
+    let newSentence = sentence.split(" ")
+    const word = newSentence.shift()
+    let endOfArray = newSentence.slice(0)
+    let firstWord = word.charAt(0).toUpperCase()+word.slice(1)
+      finalArray.push(firstWord)
+      const exception = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+
+    endOfArray.forEach(word => {
+      if(exception.includes(word)){
+        finalArray.push(word)
+      }
+      else{
+        const string = word.charAt(0).toUpperCase()+word.slice(1)
+        finalArray.push(string)
+      }
+    })
+  return finalArray.join(" ")
+  }*/
 }
